664007da950d85318050a24c29d93e925a961e9a
Zakerous76 <m.z.humayoon@gmail.com>
2024-04-25 17:49:39 +0300

    changed the minSdk to api level 27 and removed .png files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.pictospeech"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
