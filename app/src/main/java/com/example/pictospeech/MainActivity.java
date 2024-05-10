5ab2bf15c4794ce0f82eb0f2cccd9b93c2c7a45e
Zakerous76 <m.z.humayoon@gmail.com>
2024-05-10 19:50:00 +0300

    fixed the statusbar color

--- app/src/main/java/com/example/pictospeech/MainActivity.java
+++ app/src/main/java/com/example/pictospeech/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the status bar color
+
         setContentView(R.layout.main_activity);
         optionsBtn = findViewById(R.id.options_btn);
         scanPicBtn = findViewById(R.id.scan_a_pic_btn);
