6c4db37ddbbcab7365f9f8c26af75a28fc13bf35
Zakerous76 <m.z.humayoon@gmail.com>
2024-05-10 21:13:41 +0300

    removed unused import

--- app/src/main/java/com/example/pictospeech/UploadFromGalleryActivity.java
+++ app/src/main/java/com/example/pictospeech/UploadFromGalleryActivity.java
@@ -9,8 +9,6 @@ package com.example.pictospeech;
         import android.util.Log;
         import android.view.View;
         import android.widget.Button;
-        import android.widget.ImageView;
-        import android.widget.TextView;
         import android.widget.Toast;
 
         import androidx.annotation.NonNull;
